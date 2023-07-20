2541527c3961faf0ada91890441c174b9633b861
giovannilampis <giolampis@hotmail.com>
2023-07-20 17:13:58 +0200

    dinamically print developers' names

--- src/pages/Developers.vue
+++ src/pages/Developers.vue
@@ -1,9 +1,25 @@
 <script>
-
+import axios from 'axios';
 export default {
     name: 'Developers',
     components: {
 
+    },
+    data() {
+        return {
+            profiles: [],
+            baseUrlApi: 'http://127.0.0.1:8000/api/'
+        }
+    },
+    mounted() {
+        this.getProfiles();
+    },
+    methods: {
+        getProfiles(){
+            axios.get(`${this.baseUrlApi}profiles`).then(res=>{
+                this.profiles = res.data.profiles
+            })
+        }
     },
 }
 
@@ -11,6 +27,16 @@ export default {
 
 <template>
     <h1 class="text-center text-success">Sono la pagina Developers</h1>
+
+    <div class="card_container mb-5">
+    <div class="row gy-5">
+      <div v-for="(element,index) in this.profiles" :key="index" class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+        <div>{{ element.name }}</div>
+      </div>
+
+    </div>
+  </div>
+
 </template>
 
 <style lang="scss"></style>
