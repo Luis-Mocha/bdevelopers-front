462d7cf3ec18b7ff6588d73140bfd3922305d27e
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-07-21 18:33:35 +0200

    range filtro n_reviews

--- src/pages/Developers.vue
+++ src/pages/Developers.vue
@@ -11,12 +11,16 @@ export default {
             baseUrlApi: 'http://127.0.0.1:8000/api/',
             baseUrlStorage: 'http://127.0.0.1:8000/storage/',
             fields: [],
-            selectedFields: []
+            selectedFields: [],
+            reviews: [],
+            selectReviews: [],
+            selectNumbReviews: 0
         }
     },
     mounted() {
         this.getProfiles();
         this.getFields();
+        this.getReviews
     },
     watch: {
         selectedFields: {
@@ -44,6 +48,12 @@ export default {
             axios.get(`${this.baseUrlApi}fields`).then(res => {
                 this.fields = res.data.fields
             })
+        },
+
+        getReviews(){
+            axios.get(`${this.baseUrlApi}reviews`).then(res => {
+                this.reviews = res.data.reviews
+            })
         }
     },
 }
@@ -52,6 +62,9 @@ export default {
 
 <template>
     <h1 class="text-center text-success">Sono la pagina Developers</h1>
+    <div class="container">
+        <h4>Filtro specializzazione</h4>
+    </div>
     <div v-for="(elem, index) in this.fields" :key="index" class="container">
         <div class="form-check">
             <input class="form-check-input" :name="elem.id" type="checkbox" :value="elem.id" :id="`field-${elem.id}`"
@@ -60,6 +73,19 @@ export default {
                 {{ elem.name }}
             </label>
         </div>
+    </div>
+    <div class="container">
+        <h4>Filtro per numero di recensioni</h4>
+        <label for="n-reviews">Filtro per numero di recensioni</label><br />
+        <input type="range" id="n-reviews" name="n-reviews" list="options" step="5" min="0" max="20"  v-model="selectNumbReviews"/>
+
+        <datalist id="options">
+            <option  value="0" label="0+"></option>
+            <option value="5" label="5+"></option>
+            <option value="10" label="10+"></option>
+            <option value="15" label="15+"></option>
+            <option value="20" label="20+"></option>
+        </datalist>
     </div>
     
     <div class="container">
@@ -88,4 +114,23 @@ export default {
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+
+datalist {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  writing-mode: vertical-lr;
+  width: 200px;
+}
+
+option {
+  padding: 0;
+}
+
+input[type="range"] {
+  width: 200px;
+  margin: 0;
+}
+
+</style>
