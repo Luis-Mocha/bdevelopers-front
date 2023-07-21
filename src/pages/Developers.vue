8ec99c88414885d09e1332262a0397c1864ea1e3
Lucianor01 <lucianorosario02@gmail.com>
2023-07-21 12:31:44 +0200

    methods getFields

--- src/pages/Developers.vue
+++ src/pages/Developers.vue
@@ -9,16 +9,24 @@ export default {
         return {
             profiles: [],
             baseUrlApi: 'http://127.0.0.1:8000/api/',
-            baseUrlStorage: 'http://127.0.0.1:8000/storage/'
+            baseUrlStorage: 'http://127.0.0.1:8000/storage/',
+            fields: [],
+            selectedFields: []
         }
     },
     mounted() {
         this.getProfiles();
+        this.getFields();
     },
     methods: {
-        getProfiles(){
-            axios.get(`${this.baseUrlApi}profiles`).then(res=>{
+        getProfiles() {
+            axios.get(`${this.baseUrlApi}profiles`).then(res => {
                 this.profiles = res.data.profilesData
+            })
+        },
+        getFields() {
+            axios.get(`${this.baseUrlApi}fields`).then(res => {
+                this.fields = res.data.fields
             })
         }
     },
@@ -28,28 +36,35 @@ export default {
 
 <template>
     <h1 class="text-center text-success">Sono la pagina Developers</h1>
-
+    <div v-for="(elem, index) in this.fields" :key="index" class="container">
+        <div class="form-check">
+            <input class="form-check-input" :name="elem.id" type="checkbox" :value="elem.id" :id="`field-${elem.id}`">
+            <label class="form-check-label" :for="`field-${elem.id}`">
+                {{ elem.name }}
+            </label>
+        </div>
+    </div>
     <div class="container">
-      <div v-for="(element,index) in this.profiles" :key="index">        
-        <!-- Card -->
-        <div class="card" style="width: 18rem;">
-            <img :src="`${baseUrlStorage}${element.profile_image}`" alt="" class="card-img-top">
-            <div class="card-body">
-                <div>{{ element.name }}</div>
-                <div>{{ element.surname }}</div>
-                <div>{{ element.birth_date }}</div>
-                <div>{{ element.address }}</div>
-                <div>{{ element.phone_number }}</div>
-                <div>{{ element.email }}</div>
-                <a>{{ element.github_url }}</a><br>
-                <a>{{ element.linkedin_url }}</a><br>
-                <a :href="`${baseUrlStorage}${element.curriculum}`" download target="_blank" >Scarica il tuo curriculum</a>
-                <div v-for="(elem, index) in element.technology_names" :key="index">{{ elem }}</div>
+        <div v-for="(element, index) in this.profiles" :key="index">
+            <!-- Card -->
+            <div class="card" style="width: 18rem;">
+                <img :src="`${baseUrlStorage}${element.profile_image}`" alt="" class="card-img-top">
+                <div class="card-body">
+                    <div>{{ element.name }}</div>
+                    <div>{{ element.surname }}</div>
+                    <div>{{ element.birth_date }}</div>
+                    <div>{{ element.address }}</div>
+                    <div>{{ element.phone_number }}</div>
+                    <div>{{ element.email }}</div>
+                    <a>{{ element.github_url }}</a><br>
+                    <a>{{ element.linkedin_url }}</a><br>
+                    <a :href="`${baseUrlStorage}${element.curriculum}`" download target="_blank">Scarica il tuo
+                        curriculum</a>
+                    <div v-for="(elem, index) in element.technology_names" :key="index">{{ elem }}</div>
+                </div>
             </div>
         </div>
-      </div>
     </div>
-
 </template>
 
 <style lang="scss"></style>
