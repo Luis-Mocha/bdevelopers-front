f52358a2382c86c838af52b0f6d2b09e0624d6f4
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-07-21 12:01:13 +0200

    Card developers su vue

--- src/pages/Developers.vue
+++ src/pages/Developers.vue
@@ -8,7 +8,8 @@ export default {
     data() {
         return {
             profiles: [],
-            baseUrlApi: 'http://127.0.0.1:8000/api/'
+            baseUrlApi: 'http://127.0.0.1:8000/api/',
+            baseUrlStorage: 'http://127.0.0.1:8000/storage/'
         }
     },
     mounted() {
@@ -29,8 +30,23 @@ export default {
     <h1 class="text-center text-success">Sono la pagina Developers</h1>
 
     <div class="container">
-      <div v-for="(element,index) in this.profiles" :key="index">
-        <div>{{ element.name }}</div>
+      <div v-for="(element,index) in this.profiles" :key="index">        
+        <!-- Card -->
+        <div class="card" style="width: 18rem;">
+            <img :src="`${baseUrlStorage}${element.profile_image}`" alt="" class="card-img-top">
+            <div class="card-body">
+                <div>{{ element.name }}</div>
+                <div>{{ element.surname }}</div>
+                <div>{{ element.birth_date }}</div>
+                <div>{{ element.address }}</div>
+                <div>{{ element.phone_number }}</div>
+                <div>{{ element.email }}</div>
+                <a>{{ element.github_url }}</a><br>
+                <a>{{ element.linkedin_url }}</a><br>
+                <a :href="`${baseUrlStorage}${element.curriculum}`" download target="_blank" >Scarica il tuo curriculum</a>
+                <div v-for="(elem, index) in element.technology_names" :key="index">{{ elem }}</div>
+            </div>
+        </div>
       </div>
     </div>
 
