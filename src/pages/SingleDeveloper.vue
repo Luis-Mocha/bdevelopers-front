6f7b669da326074b990dd45dc3c53b0bb1e7ac86
Lucianor01 <lucianorosario02@gmail.com>
2023-07-24 11:41:44 +0200

    show reviews

--- src/pages/SingleDeveloper.vue
+++ src/pages/SingleDeveloper.vue
@@ -17,25 +17,25 @@ export default {
     //     this.getSingleProfile();
     // },
     methods: {
-        
+
         getSingleProfile() {
 
             axios.get(`${this.baseUrlApi}profiles/${this.$route.params.slug}`)
-            .then((response) => {
-                console.log(response.data.profile);
+                .then((response) => {
+                    console.log(response.data.profile);
 
-                this.singleProfile = response.data.profile;
-                
-                
-            } // sintassi try and catch
-            , error => {
-                if (error.response.status === 404) {
-                    // console.log(error.reponse.status);
-                    this.$router.push({ name: 'notFound' })
-                } else {
-                    // qualunque altro errore
-                }
-            })
+                    this.singleProfile = response.data.profile;
+
+
+                } // sintassi try and catch
+                    , error => {
+                        if (error.response.status === 404) {
+                            // console.log(error.reponse.status);
+                            this.$router.push({ name: 'notFound' })
+                        } else {
+                            // qualunque altro errore
+                        }
+                    })
         },
     },
 }
@@ -43,9 +43,8 @@ export default {
 </script>
 
 <template>
-
     <h1 class="text-center text-success">Show Developer</h1>
-    
+
     <div class="container">
         <img :src="`${baseUrlStorage}${singleProfile.profile_image}`" alt="">
 
@@ -57,14 +56,19 @@ export default {
         <div> {{ singleProfile.github_url }} </div>
         <div> {{ singleProfile.linkedin_url }} </div>
         <div> {{ singleProfile.performance }} </div>
-        <a :href="`${baseUrlStorage}${singleProfile.curriculum}`" target="_blank" rel="noopener noreferrer">Scarica il curriculum</a>
+        <a :href="`${baseUrlStorage}${singleProfile.curriculum}`" target="_blank" rel="noopener noreferrer">Scarica il
+            curriculum</a>
+        <h2 class="mt-4 mb-2">RECENSIONI</h2>
+        <ul>
+            <div v-for="(elem, index) in singleProfile.review_desc" :key="index">
 
+                <li>{{ elem }}</li>
+                <!-- <div>{{ elem.review_name }}</div> -->
+                <!-- <div>{{ elem.review_name }}</div> -->
+            </div>
+        </ul>
 
     </div>
-
 </template>
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
