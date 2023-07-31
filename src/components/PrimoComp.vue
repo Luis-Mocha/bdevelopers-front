ad4af08847df22600e3780d7034dc0f66c306263
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-07-31 13:09:55 +0200

    creazione componenti vue

---
+++ src/components/PrimoComp.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    name: 'PrimoComp',
+}
+
+</script>
+
+
+<template>
+   <div id="section">
+    <h1>prova</h1>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+    #section{
+        height: 100vh;
+        background-color: red;
+    }
+</style>
---
+++ src/components/QuintoComp.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    name: 'SecondoComp',
+}
+
+
+</script>
+
+
+<template>
+   <div id="section">
+    <h1>secondo componente</h1>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+    #section{
+        height: 100vh;
+        background-color: green;
+    }
+</style>
